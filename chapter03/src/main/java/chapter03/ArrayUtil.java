9d1a50d8db3c5b7c609327a2d8fdd8882d788f2e
alswlals <awlals616@gmail.com>
2022-12-21 21:49:34 +0900

    대박드디어해냄 concat 다 뿌셔

--- chapter03/src/main/java/chapter03/ArrayUtil.java
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -7,10 +7,32 @@ public class ArrayUtil {
 		
 		// error: double d = result + 10;
 		
-		for(int i = 0; i < is.length; i++) {
-			
+		for(int i = 0; i < result.length; i++) {
+			result[i] = is[i] ;
 		}
 		return result;
+	}
+
+	public static int[] doubleToInt(double[] ds) {
+		int[] da = new int[ds.length];
+		for(int i = 0; i < da.length; i++) {
+			ds[i] = da[i];
+		}
+		return da;
+	}
+
+	public static int[] concat(int[] is, int[] is2) {
+		int[] a = new int[is.length+is2.length];
+		int index = 0;
+		
+		for(int i = 0; i<is.length; i++) {
+			a[index++]=is[i];
+		}
+		for(int i = 0; i<is2.length; i++) {
+			a[index++]=is2[i];
+		}
+		
+		return a;
 	}
 
 }
