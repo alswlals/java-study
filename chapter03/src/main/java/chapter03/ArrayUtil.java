facc2ad73697c1b0733345b0b61c2f9413a1c932
승학산-14 <-14@DESKTOP-PC0C2IR>
2022-12-21 14:43:56 +0900

    ArrayUtil #array #배열

---
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class ArrayUtil {
+
+	public static double[] intToDouble(int[] is) {
+		double[] result = new double[is.length]; //-> 초기화 시켜줘야함 아니면 워닝뜸
+		
+		// error: double d = result + 10;
+		
+		for(int i = 0; i < is.length; i++) {
+			
+		}
+		return result;
+	}
+
+}
