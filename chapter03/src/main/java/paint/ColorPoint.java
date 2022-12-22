e0fa273b8cd80102fd9064bb3b6fc5ac6bfbe2ba
alswlals <awlals616@gmail.com>
2022-12-22 10:12:56 +0900

    override

---
+++ chapter03/src/main/java/paint/ColorPoint.java
@@ -0,0 +1,19 @@
+package paint;
+
+public class ColorPoint extends Point {
+	String color;
+
+	public String getColor() {
+		return color;
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		super.show();
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -3,7 +3,7 @@ package paint;
 public class Main {
 
 	public static void main(String[] args) {
-		Point point = new Point(10, 10);
+		Point point1 = new Point(10, 10);
 //		Point.java에서 
 //		public Point(int x, int y) {
 //		this.x = x;
@@ -15,11 +15,27 @@ public class Main {
 		
 //		point.setX(10);
 //		point.setY(10);
-		point.show();
+//		point.show();
+//		
+//		point.show(true);
 		
-		point.show(true);
-		point.show(false);
+		drawPoint(point1); //아래 static drawrPoint 구현 후 진행
+		point1.show(false);
 //		point.disapear();
+		
+		ColorPoint point2 = new ColorPoint();
+		point2.setX(20);
+		point2.setY(20);
+		point2.setColor("red");
+		drawColorPoint(point2);
+	}
+	
+	public static void drawPoint(Point point) {
+		point.show();
+	}
+	
+	public static void drawColorPoint(ColorPoint colorpoint) {
+		colorpoint.show();
 	}
 
 }
