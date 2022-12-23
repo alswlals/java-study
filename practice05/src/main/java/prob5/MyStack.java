5664d08cc9fa04bed66bab8eb17c0e405c047b49
alswlals <awlals616@gmail.com>
2022-12-23 20:20:17 +0900

    pratice02 #05

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,10 +1,33 @@
 package prob5;
-//stream으로 구혀 ㄴ? 
+//String으로 구현 
 public class MyStack {
-	private String[] buffer;
 
-	public String pop() {
-		// TODO Auto-generated method stub
-		return null;
+	private String[] buffer;
+	private int arr;
+	
+	public MyStack(int i) {
+		buffer = new String[i * 2];
+		arr = -1;
 	}
+
+	public void push(String string) {
+		arr += 1;
+		buffer[arr] = string;
+//		System.out.println(buffer[arr]);
+		
+	}
+
+	public String pop() throws MyStackException {
+		if(arr == -1) {
+			throw new MyStackException();
+		}
+		String s = buffer[arr];
+		arr -= 1;
+		return s;
+	}
+
+	public boolean isEmpty() {
+		return arr ==-1 ? true : false;
+	}
+	
 }
