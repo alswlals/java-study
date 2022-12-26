7794896193ae41e69132d4ed392098a439ab624a
alswlals <awlals616@gmail.com>
2022-12-26 21:25:42 +0900

    final

--- finaltest/src/main/java/prob03/Money.java
+++ finaltest/src/main/java/prob03/Money.java
@@ -3,29 +3,45 @@ package prob03;
 public class Money {
 	private int amount;
 
-	public Money(int i) {
-		// TODO Auto-generated constructor stub
+	public Money(int amount) {
+		this.amount = amount;
 	}
 
-	public Object add(Money three) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object add(Money money) {
+		Money m = new Money(0);
+		m.amount = this.amount + money.amount;
+		return m;
 	}
 
-	public Object multiply(Money two) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object multiply(Money money) {
+		Money m = new Money(0);
+		m.amount = this.amount * money.amount;
+		return m;
 	}
 
-	public Object minus(Money two) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object minus(Money money) {
+		Money m = new Money(0);
+		m.amount = this.amount - money.amount;
+		return m;
 	}
 
-	public Object devide(Money five) {
-		// TODO Auto-generated method stub
-		return null;
+	public Money devide(Money money) {
+		Money m = new Money(0);
+		m.amount = this.amount/money.amount;
+		return m;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Money) {
+			return this.amount==((Money) obj).amount;
+		}
+		return false;
+		
 	}
 	
 	/* 코드 작성 */
+	
+	
 }
--- finaltest/src/main/java/prob04/Person.java
+++ finaltest/src/main/java/prob04/Person.java
@@ -6,4 +6,30 @@ public class Person {
 	private String name;
 	
 	/* 코드 작성 */
+	
+	public Person() {
+		this.age = 12;
+		this.name = "";
+		numberOfPerson += 1;
+	}
+
+	public Person(String name) {
+		this.age = 12;
+		this.name = name;
+		numberOfPerson += 1;
+	}
+
+	public Person(int age, String name) {
+		this.age = age;
+		this.name = name;
+		numberOfPerson += 1;
+	}	
+
+	public static int getPopulation() {
+		return numberOfPerson;
+	}
+
+	public void selfIntroduce() {
+		System.out.println("내 이름은 "+name+"이며, 나이는 "+age+"살 입니다.");		
+	} 
 }
