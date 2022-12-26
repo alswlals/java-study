bbc807e8be42e0cacaa7d27100eedabc9c8ff288
alswlals <awlals616@gmail.com>
2022-12-26 09:37:20 +0900

    Generic class

---
+++ practice05/src/main/java/prob5/MainApp03.java
@@ -0,0 +1,33 @@
+package prob5;
+//stack 만드는 문제
+//배열 사이즈 2배 정도 늘리기(새배열 만들고 늘린담에 버퍼 이용해 복사, 연결시키기)
+//pop, 
+public class MainApp03 {
+
+	public static void main(String[] args) {
+		try{MyStack03<String> stack = new MyStack03<>(3);
+		stack.push("Hello");
+		stack.push("World");
+		stack.push("!!!");
+		stack.push("java");
+		stack.push(".");
+
+		while (stack.isEmpty() == false) {
+			String s = stack.pop();
+			System.out.println( s );
+		}
+
+		System.out.println("======================================");
+
+		stack = new MyStack03<>(3);
+		stack.push("Hello");
+
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+
+		}catch(MyStackException ex) {
+			System.out.println(ex);
+		}
+	}
+
+}
---
+++ practice05/src/main/java/prob5/MyStack03.java
@@ -0,0 +1,44 @@
+package prob5;
+//Generic 사용
+public class MyStack03<T> {
+	private int top;
+	private T[] buffer;
+	
+	@SuppressWarnings("unchecked")
+	public MyStack03(int i) {
+		top = -1;
+		buffer = (T[])new Object[i];
+	}
+
+	public void push(T t) {
+		if(top == buffer.length - 1) {
+			resize();
+		}
+		buffer[++top] = t;
+		
+	}
+
+	public T pop() throws MyStackException {
+		if(top == -1) {
+			throw new MyStackException();
+		}
+		T s = buffer[top];
+		top -= 1;
+		return s;
+	}
+
+	public boolean isEmpty() {
+		return top ==-1;
+	}
+	
+	private void resize() {
+		@SuppressWarnings("unchecked")
+		T[] temp =(T[]) new Object[buffer.length *2];
+		for(int i = 0; i <= top; i++) {
+			temp[i] = buffer[i];
+		}
+		buffer = temp;
+	}
+	
+	
+}
