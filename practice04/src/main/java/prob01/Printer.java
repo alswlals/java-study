1d8615a6d820469af9bf9c85a9dffa4686c8840f
alswlals <awlals616@gmail.com>
2022-12-26 09:53:46 +0900

    Generic + 가변

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,19 +1,39 @@
 package prob01;
 
 public class Printer {
-	public void println(int num) {
-		System.out.println(num);
+//	public void println(int num) {
+//		System.out.println(num);
+//	}
+//
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String string) {
+//		System.out.println(string);
+//	}
+//	
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
-
-	public void println(boolean b) {
-		System.out.println(b);
+	
+	public int sum(Integer... nums) {
+		int s = 0;
+		for(Integer n : nums) {
+			s += n;
+		}
+		return s;
 	}
-
-	public void println(double d) {
-		System.out.println(d);
+	
+	public <T> void println(T... ts) {
+		for(T tt : ts) {
+			System.out.println(tt);
+		}
+		
 	}
-
-	public void println(String string) {
-		System.out.println(string);
-	}
+	
 }
