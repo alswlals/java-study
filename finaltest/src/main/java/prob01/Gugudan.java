49a4807df75e79d70df6b3984851a5cd8905554c
alswlals <awlals616@gmail.com>
2022-12-26 19:41:29 +0900

    help

---
+++ finaltest/src/main/java/prob01/Gugudan.java
@@ -0,0 +1,13 @@
+package prob01;
+
+public class Gugudan {
+	private int lValue;
+	private int rValue;
+	
+	//set? 
+	// equals override 사용
+	public Gugudan(int lValue, int rValue) {
+		this.lValue = lValue;
+		this.rValue = rValue;
+	}
+}
--- practice02/src/main/java/prob05/Prob05.java
+++ practice02/src/main/java/prob05/Prob05.java
@@ -11,40 +11,34 @@ public class Prob05 {
 		while( true ) {
 			
 			/* 게임 작성 */
+			int count = 1;
+			int number = 0;
 			
 			// 정답 랜덤하게 만들기
 			Random random = new Random();
 			int correctNumber = random.nextInt( 100 ) + 1;
-			System.out.println(correctNumber);
-
+//			System.out.println(correctNumber);
+			System.out.println("수를 결정하였습니다. 맞추어 보세요");
+			
 			while(true) {
 				System.out.println("1-100");
 				System.out.print("1>>");
 				int answer = scanner.nextInt();
 				
-				for(int i=0;; i++) {
+				for(int i=0; i++;) {
 					if(answer > correctNumber) {
 						System.out.println("더 낮게");
 					} else if(answer < correctNumber) {
 						System.out.println("더 높게");
-					} else if(answer == correctNumber) {
-						System.out.print( "다시 하겠습니까(y/n)>>" );
-						String answer1 = scanner.next();
-						if( "y".equals( answer1 ) == false ) {
-							break;
 						}
 					}
 				}
-//				while(answer != correctNumber) {
-//				if (answer < correctNumber) 
-//					System.out.print("더 크게");
-//				else if(answer > correctNumber)
-//					System.out.print("더 작게");
-//				
-//					break;
-//				}
 			}
 			//새 게임 여부 확인하기
+		System.out.print( "다시 하겠습니까(y/n)>>" );
+		String answer1 = scanner.next();
+		if( "y".equals( answer1 ) == false ) {
+			break;
 		}
 		scanner.close();
 	}
