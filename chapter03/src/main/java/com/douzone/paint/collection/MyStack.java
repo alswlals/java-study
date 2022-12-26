20f1ba405a232c1d6ffe7cb6c2356ac84f108549
alswlals <awlals616@gmail.com>
2022-12-26 11:25:47 +0900

    generic parameter unbounded

---
+++ chapter03/src/main/java/com/douzone/paint/collection/MyStack.java
@@ -0,0 +1,47 @@
+package com.douzone.paint.collection;
+
+import com.douzone.paint.shape.Shape;
+
+//Generic 사용
+public class MyStack<T super Shape> {
+	private int top;
+	private T[] buffer;
+	
+	@SuppressWarnings("unchecked")
+	public MyStack(int i) {
+		top = -1;
+		buffer = (T[])new Object[i];
+	}
+
+	public void push(T t) {
+		if(top == buffer.length - 1) {
+			resize();
+		}
+		buffer[++top] = t;
+		
+	}
+
+	public T pop() throws MyStackException {
+		if(top == -1) {
+			throw new MyStackException();
+		}
+		T s = buffer[top];
+		top -= 1;
+		return s;
+	}
+
+	public boolean isEmpty() {
+		return top ==-1;
+	}
+	
+	private void resize() {
+		@SuppressWarnings("unchecked")
+		T[] temp =(T[]) new Object[buffer.length *2];
+		for(int i = 0; i <= top; i++) {
+			temp[i] = buffer[i];
+		}
+		buffer = temp;
+	}
+	
+	
+}
---
+++ chapter03/src/main/java/com/douzone/paint/collection/MyStackException.java
@@ -0,0 +1,8 @@
+package com.douzone.paint.collection;
+
+public class MyStackException extends Exception {
+	public MyStackException() {
+		super("stack is empty");
+	}
+	
+}
