ddaeabd40a58b076adbab2674e2934b4c6116fad
alswlals <awlals616@gmail.com>
2022-12-29 13:13:56 +0900

    Echo #finish

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -1,14 +1,9 @@
 package echo;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class EchoServer {
 	/* 넣는 이유 : */
@@ -22,45 +17,14 @@ public class EchoServer {
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", 8000));
 			log("starts..... [port: " + PORT + "]");
 
-			Socket socket = serverSocket.accept();
-
-			//여기서부터 스레드 가넝
-			/* 정보 빼오기 */
-			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
-			int remotePort = inetRemoteSocketAddress.getPort();
-			log("connected by client[" + remoteHostAddress + ":" + remotePort + "]");
-
-			try {
-				/*
-				 * BufferedReader로 보내주기 때문에 개행 필수(경계 | 문자 x) 읽을 수 있게 보장은 해주지만 그대로 읽게는 안해줌 true
-				 * 사용 이유: 버퍼를 사용할 때 즉시 비워줘라(다 찰때까지 기다리지 말고)
-				 */
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
-
-				while (true) {
-					String data = br.readLine(); /* 꼭 개행을 붙여라 */
-					if (data == null) {
-						log("closed by client");
-						break;
-					}
-					log("received:  " + data);
-					pw.println(data + "\n");
-				}
-			} catch (SocketException ex) {
-				System.out.println("[server]suddenly closed by client");
-			} catch (IOException ex) {
-				log("error: " + ex);
-			} finally {
-				try {
-					if (socket != null && !socket.isClosed()) {
-						socket.close();
-					}
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
+			while(true) {
+				Socket socket = serverSocket.accept();
+				/*EchoRequestHandler 파일에서 socket 정보를 받아옴*/
+				new EchoRequestHandler(socket).start();
 			}
+			//여기서부터 스레드 가넝
+			
+			
 			//여기까지 스레드 가넝
 		} catch (IOException e) {
 			log("error: " + e);
@@ -77,6 +41,6 @@ public class EchoServer {
 	}
 
 	private static void log(String message) {
-		System.out.println("[EchoServer] " + message);
+		System.out.println("[EchoServer#" + Thread.currentThread().getId()+ "] " + message);
 	}
 }
